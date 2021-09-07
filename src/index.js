9a0e736064b157068f1c0c7f2109259578d31c6c
bthank <bthankachan@yahoo.com>
2021-09-07 12:31:40 -0400

    jsx learning

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//const buttonText = "1234567";
+const buttonText = {text: "Click Me"};
+function getButtonText(){
+    return "Click on me!";
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const labelText = 'Enter name:';
+
+// Create React components
+//const App = function() {
+const App = () => {
+    // this is JSX code
+    return (<div>
+        <label className="label" htmlFor="name">
+            {labelText}
+        </label>
+        <input id="name" type="text" />
+        <button style={{"backgroundColor": "blue", "color": "white"}}>
+            {buttonText.text}
+        </button>
+    </div>);
+};
+
+
+
+// Take the React component and show it on the screen
+ReactDOM.render(<App />,document.getElementById("root"));
+
